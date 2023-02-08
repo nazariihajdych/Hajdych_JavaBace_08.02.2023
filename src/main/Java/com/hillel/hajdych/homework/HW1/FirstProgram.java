e93ed89ff088e34b9682d49ce834db18ab445733
Nazarii Hajdych <wertyp.poiteq@gmail.com>
2023-02-08 16:54:45 +0200

    first program homework 1

---
+++ src/main/Java/com/hillel/hajdych/homework/HW1/FirstProgram.java
@@ -0,0 +1,7 @@
+package com.hillel.hajdych.homework.HW1;
+
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello world, my name is Nazarii!");
+    }
+}
