f5e62e8352b1eb12e85da76597cf45bf8fd67cec
Nazarii Hajdych <wertyp.poiteq@gmail.com>
2023-02-08 17:58:41 +0200

    changes for pull

--- src/main/Java/com/hillel/hajdych/homework/HW3/Main.java
+++ src/main/Java/com/hillel/hajdych/homework/HW3/Main.java
@@ -3,6 +3,7 @@ package com.hillel.hajdych.homework.HW3;
 public class Main {
   public static void main(String[] args){
   
-    System.out.println("Changes fron GitHub");
+    System.out.println("Changes from GitHub");
+    System.out.println("Changes for Pull");
   }
 }
