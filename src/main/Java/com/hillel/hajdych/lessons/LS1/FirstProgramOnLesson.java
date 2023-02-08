19689805615a5795d9c5180b5a062f4620fa4210
Nazarii Hajdych <wertyp.poiteq@gmail.com>
2023-02-08 16:54:02 +0200

    first program on lesson 1

---
+++ src/main/Java/com/hillel/hajdych/lessons/LS1/FirstProgramOnLesson.java
@@ -0,0 +1,10 @@
+package com.hillel.hajdych.lessons.LS1;
+
+public class FirstProgramOnLesson {
+    public static void main(String[] args) {
+        System.out.println("Hello word");
+        System.out.println("Hello word");
+
+        System.out.println("Hello");
+    }
+}
