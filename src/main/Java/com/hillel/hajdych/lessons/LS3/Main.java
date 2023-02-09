6f4a7ce01320dbb2ee2414ff8d7f58562533b94f
Nazarii Hajdych <wertyp.poiteq@gmail.com>
2023-02-09 20:52:12 +0200

    lesson 3 first after break; Scanner scanner = new Scanner(System.in); Задачка, зчитування даних.

--- src/main/Java/com/hillel/hajdych/lessons/LS3/Main.java
+++ src/main/Java/com/hillel/hajdych/lessons/LS3/Main.java
@@ -1,5 +1,7 @@
 package com.hillel.hajdych.lessons.LS3;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -57,20 +59,67 @@ public class Main {
 //        System.out.println(Math.round(10.5));
 //        System.out.println(Math.round(10.4));
 
-        System.out.println(Math.random()); // [0.0 1.0) -> 0 .... 0.9999999
+//        System.out.println(Math.random()); // [0.0 1.0) -> 0 .... 0.9999999
+//
+//        System.out.println(Math.round(Math.random()*10));
+//        System.out.println((int) (Math.random() * 11));
+//
+//        // 15 - 30
+//
+//        int min = 15;
+//        int max = 30;
+//
+//        System.out.println(15 + (int) (Math.random() * 16));
+//        System.out.println(min + (int)(Math.random() * (max - min + 1)));
 
-        System.out.println(Math.round(Math.random()*10));
-        System.out.println((int) (Math.random() * 11));
 
-        // 15 - 30
+        //Zarplaty
+//        int emp1 = 700;
+//        int emp2 = 1500;
+//        int emp3 = 3500;
+//
+//        int monthAndYear = 10 * 12;
+//
+//        int salEmp1for10Y = emp1 * monthAndYear;
+//        int salEmp2for10Y = emp2 * monthAndYear;
+//        int salEmp3for10Y = emp3 * monthAndYear;
+//
+//        double tax = 0.95;
+//
+//        double sal1OutTax = salEmp1for10Y * tax;
+//        double sal2OutTax = salEmp2for10Y * tax;
+//        double sal3OutTax = salEmp3for10Y * tax;
+//
+//        double avgSal = ((salEmp1for10Y + salEmp2for10Y + salEmp3for10Y) / 3.0);
+//
+//        double avgSalOutTax = ((sal1OutTax + sal2OutTax + sal3OutTax) / 3.0);
+//
+//        System.out.println("ЗП 10 років Працівник 1: " + salEmp1for10Y);
+//        System.out.println("ЗП 10 років Працівник 2: " + salEmp2for10Y);
+//        System.out.println("ЗП 10 років Працівник 3: " + salEmp3for10Y);
+//
+//        System.out.println();
+//
+//        System.out.println("ЗП 10 років Працівник 1 - 5% податку: " + sal1OutTax);
+//        System.out.println("ЗП 10 років Працівник 2 - 5% податку: " + sal2OutTax);
+//        System.out.println("ЗП 10 років Працівник 3 - 5% податку: " + sal3OutTax);
+//
+//        System.out.println();
+//
+//        System.out.println("Середня зарплата за 10 років: " + avgSal);
+//        System.out.println("Середня зарплата за 10 років - 5%: " + avgSalOutTax);
 
-        int min = 15;
-        int max = 30;
 
-        System.out.println(15 + (int) (Math.random() * 16));
-        System.out.println(min + (int)(Math.random() * (max - min + 1)));
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Please enter some string: ");
+        String str = scanner.next();
 
+        System.out.println("str = " + str);
+
+        // .next Зчитує до пробілу. При тому якщо декілька введень з пробілом вони запису
+        //ються в буфер. Наступний некст зчитає з буферу запис після пробілу.
+        //.nextLine зчитує посністю строку, записує в буфер після переходу на новий рядок
 
 
 
