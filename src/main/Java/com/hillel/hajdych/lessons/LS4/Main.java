fa9dfaab86edad60e9aff5c64c5205edd393e3c1
Nazarii Hajdych <wertyp.poiteq@gmail.com>
2023-02-13 20:15:39 +0200

    homework 4, chinese dynasty armies

---
+++ src/main/Java/com/hillel/hajdych/lessons/LS4/Main.java
@@ -0,0 +1,75 @@
+package com.hillel.hajdych.lessons.LS4;
+
+public class Main {
+    public static void main(String[] args) {
+//        int a = 10;
+//        int b = 5;
+//        int c = 7;
+//
+//
+//        System.out.println(a > b);
+//        System.out.println(a >= b);
+//        System.out.println(a < b);
+//        System.out.println(a <= b);
+//
+//        System.out.println(a == b);
+//        System.out.println(a != b);
+//
+//        System.out.println(a > b && b > c);
+//        System.out.println(a > b || b > c);
+//
+//        System.out.println(a > b || b > c && c <= 12);
+//
+//        System.out.println(10 > 20 && (20 < 30 || true ));
+
+//        int a = 10;
+//        int b = 5;
+//
+//        System.out.println(a < b && b++ >= 5); // права частине не запучкається якщо ліва false для оператора &&
+//
+//        System.out.println("a = " + a);
+//        System.out.println("b = " + b);
+
+        // ------------------------------//
+
+//        int a = 10;
+//        int b = 5;
+//
+//        if (a > b) {
+//            System.out.println("a > b");
+//        }
+//
+//
+//        if (a > b) {
+//            System.out.println("True");
+//        } else {
+//            System.out.println(false);
+//        }
+//
+//
+//
+//        if (a > b){
+//            System.out.println(true);
+//        } else if (a == b) {
+//            System.out.println(true + "else-if");
+//        } else {
+//            System.out.println(false);
+//        }
+
+
+        int a = 10;
+        int b = 5;
+
+        //Тернарний оператор заміна if, вкладеність тернарних операторів теж можлива
+
+        System.out.println(a >= b ? "a >=b" : "a < b");
+                        //умова if тіло else тіло
+
+
+
+
+
+
+
+    }
+}
