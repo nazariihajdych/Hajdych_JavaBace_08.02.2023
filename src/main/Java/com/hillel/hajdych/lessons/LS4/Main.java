7d746fb295ff0fd2f53ff00ba255f40de15e2e40
Nazarii Hajdych <wertyp.poiteq@gmail.com>
2023-02-13 20:46:54 +0200

    switch case 2

--- src/main/Java/com/hillel/hajdych/lessons/LS4/Main.java
+++ src/main/Java/com/hillel/hajdych/lessons/LS4/Main.java
@@ -89,24 +89,21 @@ public class Main {
 
         int a = 3;
 
-        switch (a){
-            case 1 : { System.out.println("1");
-            break;
+        switch (a) {
+            case 1: {
+                System.out.println("1");
+                break;
             }
-            case 2 : {
+            case 2: {
                 System.out.println("2");
                 break;
             }
-            case 3 : {
+            case 3: {
                 System.out.println("3");
                 break;
             }
             default: {
                 System.out.println("default");
-                break;
-            }
-            case 4 : {
-                System.out.println("4");
                 break;
             }
         }
