879bcf4b58325a3fcb245a803b28b4ce80c06895
Nazarii Hajdych <wertyp.poiteq@gmail.com>
2023-02-28 01:45:01 +0200

    lesson 8

---
+++ src/main/Java/com/hillel/hajdych/lessons/LS8/Main.java
@@ -0,0 +1,16 @@
+package com.hillel.hajdych.lessons.LS8;
+
+public class Main {
+    public static void main(String[] args) {
+        // вирішення порівння зчитування!!!
+        String temp = new String("1dfg");
+        System.out.println( Integer.parseInt(temp));
+
+
+
+    }
+
+    static void printHello(){
+        System.out.println("Hello world");
+    }
+}
